f3e8e929a526936fca04687bd9960d007cef34d0
DeividBorges93 <deividmborges@gmail.com>
2022-06-02 18:49:26 -0300

    Cria tabela planos

--- desafio1.sql
+++ desafio1.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
+CREATE TABLE SpotifyClone.planos(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  tipo_plano VARCHAR(20) NOT NULL,
+  valor_plano DECIMAL(3,2) NOT NULL
+);
+
